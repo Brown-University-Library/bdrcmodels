0e64bb46115ae311c5e5ee50016d82ea35675972
Ben Cail <benjamin_cail@brown.edu>
2019-05-29 10:58:24 -0400

    bump to v0.2; remove dependency_links; bump bdrxml to 1.0a1

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrcmodels',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'eulfedora>=1.7.2',
-        'bdrxml>=0.9',
-    ],
-    dependency_links=[
-        'https://github.com/Brown-University-Library/bdrxml/archive/v0.9.zip#egg=bdrxml-0.9',
+        'bdrxml @ https://github.com/Brown-University-Library/bdrxml/archive/v1.0a1.zip#sha1=5802ed82ee80a9627657cbb222fe9c056f73ad2c',
     ],
 )
