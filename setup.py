3619b87e1477d6b1fbb134ba4c888d1a08fe4ca0
Ben Cail <benjamin_cail@brown.edu>
2019-05-30 12:11:01 -0400

    bump version for development

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrcmodels',
-    version='0.4',
+    version='0.5a1',
     url='https://github.com/Brown-University-Library/bdrcmodels',
     author='Brown University Library',
     author_email='bdr@brown.edu',
