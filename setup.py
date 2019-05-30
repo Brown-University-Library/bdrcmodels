e4690292f5fe48896669848ffd3b7c51c94e3cd6
Ben Cail <benjamin_cail@brown.edu>
2019-05-30 11:44:01 -0400

    update version to 0.3, and require bdrxml >= 1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='bdrcmodels',
-    version='0.3a1',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         'eulfedora>=1.7.2',
-        'bdrxml @ https://github.com/Brown-University-Library/bdrxml/archive/v1.0a1.zip#sha1=5802ed82ee80a9627657cbb222fe9c056f73ad2c',
+        'bdrxml>=1.0',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
