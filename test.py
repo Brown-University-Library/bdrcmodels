86174cb435811534e644bec4c42ee1d8c2208b36
Ben Cail <benjamin_cail@brown.edu>
2019-10-28 14:05:56 -0400

    tiff files default to MASTER dsID

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ class GetDsIdTest(unittest.TestCase):
         self.assertEqual(models.get_dsid_from_filename('test.avi'), 'AVI')
         self.assertEqual(models.get_dsid_from_filename('test.wav'), 'AUDIO-MASTER')
         self.assertEqual(models.get_dsid_from_filename('test.aiff'), 'AUDIO-MASTER')
+        self.assertEqual(models.get_dsid_from_filename('test.tiff'), 'MASTER')
 
 
 class GetMimetypeTest(unittest.TestCase):
